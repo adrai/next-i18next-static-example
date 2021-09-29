e17443561f73a9ab5e4a83ed5b83cb172a762000
Adriano Raiano <adriano@raiano.ch>
2021-09-30 00:10:24 +0200

    next-i18next update

--- pages/_app.js
+++ pages/_app.js
@@ -2,4 +2,10 @@ import { appWithTranslation } from 'next-i18next'
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
-export default appWithTranslation(MyApp)
+const WrappedApp = appWithTranslation(MyApp)
+
+// because we do not use the i18n feature of next.js
+export default function RouterEmulatedApp ({ ...rest }) {
+  rest.router.locale = 'en'
+  return <WrappedApp {...rest} />
+}
