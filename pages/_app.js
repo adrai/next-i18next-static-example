482163559cfedfa1c147a4bafddf0bc19d6cb371
Frank Cieslik <frank.cieslik@gmail.com>
2022-06-26 17:17:54 +0200

    updated next package, fix router locale

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { appWithTranslation } from 'next-i18next'
+import { useRouter } from 'next/router'
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
 const WrappedApp = appWithTranslation(MyApp)
-
 // because we do not use the i18n feature of next.js
 export default function RouterEmulatedApp ({ ...props }) {
-  props.router.locale = props.router.query.locale
+  const router = useRouter();
+  router.locale = props.router.query.locale
+  
+  props.router = router
   return <WrappedApp {...props} />
 }
